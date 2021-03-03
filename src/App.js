22135ed5db2ff0f5cb4b5e2a484a3c24f1889085
Jardelino Medeiros <jardelinomedeiros@outlook.com>
2021-03-02 23:48:11 -0300

    criando as Seções Hero, Credit e Cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Navbar from "./componentes/Navbar";
+import Hero from "./componentes/Hero";
+import Credit from "./componentes/Credit";
+import Card from "./componentes/Card";
 import "./App.scss";
 
 
@@ -7,6 +10,9 @@ const App = () => (
   
     <div className="App"> 
       <Navbar />
+      <Hero />
+      <Credit />
+      <Card />
     </div>
   );
 
