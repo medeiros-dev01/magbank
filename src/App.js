0f5c84a8f4b0f7eceadb35072ecdc75b3af78541
Jardelino Medeiros <jardelinomedeiros@outlook.com>
2021-03-18 16:59:39 -0300

    feat(footer) implementation and styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CardList from "./componentes/CardList";
 import CenteredButton from "./componentes/CenteredButton";
 import Institutional from "./componentes/Institutional";
 import Faq from "./componentes/Faq";
+import Footer from "./componentes/Footer";
 
 import "./App.scss";
 
@@ -22,6 +23,7 @@ const App = () => (
 
     <Institutional />
     <Faq />
+    <Footer />
   </div>
 );
 
